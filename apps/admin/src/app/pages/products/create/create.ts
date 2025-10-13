c8036c9d3c258cd5660a3dd9fb59c6f9e612648c
Ömer Faruk Gözegir <gozgirfaruk@gmail.com>
2025-10-13 17:10:49 +0300

    Implement product creation form

--- apps/admin/src/app/pages/products/create/create.ts
+++ apps/admin/src/app/pages/products/create/create.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component, inject, ViewEncapsulation } from '@angular/core';
 import Blank from '../../../components/blank';
 import { Common } from '../../../services/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
-  imports: [Blank],
+  imports: [Blank,FormsModule],
   templateUrl: './create.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -12,5 +16,17 @@ export default class ProductCreate {
 readonly #common = inject(Common);
   constructor(){
     this.#common.set("Product Create","/products/create","deployed_code");
+  }
+
+  readonly #http= inject(HttpClient);
+  readonly #router = inject(Router);
+  readonly #location = inject(Location);
+
+  save(form:NgForm){
+    if(!form.valid) return;
+    this.#http.post("http://localhost:3000/products",form.value).subscribe(()=>{
+      this.#router.navigateByUrl("/products");
+      this.#location.back();
+    })
   }
 }
